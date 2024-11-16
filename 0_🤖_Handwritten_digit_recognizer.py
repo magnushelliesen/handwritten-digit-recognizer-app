a9cf912fa2d7bcc27ff3c5423ac7c83fb1dc3650
magnushelliesen <magnus.helliesen@gmail.com>
2024-11-16 13:32:42 +0100

    Added details of calculation

--- 0_🤖_Handwritten_digit_recognizer.py
+++ 0_🤖_Handwritten_digit_recognizer.py
@@ -9,6 +9,7 @@ from pathlib import Path
 import os
 from time import sleep
 from functions import get_neural_network, center_input
+import matplotlib.pyplot as plt
 
 st.set_page_config(
     initial_sidebar_state="collapsed"
@@ -46,8 +47,11 @@ drawing = st_canvas(
     key="canvas"
 )
 
+calculate = st.button("Recognize digit 👀")
+show_details = st.toggle("Show details", True)
+
 # Use neural net to recognize user input
-if st.button("Recognize digit 👀"):
+if calculate:
     X = np.mean(np.array(drawing.image_data)[:, :, :3], axis=2)
 
     X = center_input(X)
@@ -58,21 +62,21 @@ if st.button("Recognize digit 👀"):
     height = X.shape[0]
     width = X.shape[1]
 
-    #X = np.vstack((
-    #    np.full((int(height*0.15), width), 255),
-    #    X,
-    #    np.full((int(height*0.05), width), 255)
-    #))
+    X = np.vstack((
+        np.full((int(height*0.1), width), 255),
+        X,
+        np.full((int(height*0.1), width), 255)
+    ))
     
     # Pad in x direction
     height = X.shape[0]
     width = X.shape[1]
 
-    #X = np.hstack((
-    #    np.full((height, int(width*0.20)), 255),
-    #    X,
-    #    np.full((height, int(width*0.20)), 255)
-    #))
+    X = np.hstack((
+        np.full((height, int(width*0.1)), 255),
+        X,
+        np.full((height, int(width*0.1)), 255)
+    ))
 
     # Resizing input to 28 x 28
     x = matrix_mapper(X, 28, 28)
@@ -87,24 +91,38 @@ if st.button("Recognize digit 👀"):
     try:
         prediction = nn.predict(digit_norm)
     except ValueError:
-        st.error("I'm sorry, I can't make that out 😭")
+        st.error("I'm terribly sorry, I can't make that out 😭")
         st.stop()
 
     # Return guess with varying level of confidence
-    guess = sorted(zip(prediction, range(9)), reverse=True)
+    guess = sorted(zip(prediction, range(10)), reverse=True)
 
+    st.header("Best guess")
     if guess[0][0] > 0.8:
-        st.header(f"I'm pretty sure it's a {guess[0][1]} 😁")
+        st.write(f"I'm pretty sure it's a {guess[0][1]} 😁")
     elif guess[0][0] > 0.4:
-        st.header(f"It kinda looks like a {guess[0][1]} 🙂")
+        st.write(f"It kinda looks like a {guess[0][1]} 🙂")
     elif guess[0][0] > 0.2:
-        st.header(f"It could be a {guess[0][1]} 🤔")
+        st.write(f"It could be a {guess[0][1]} 🤔")
     else:
-        st.subheader(f"My best guess is a {guess[0][1]} 🫣")
+        st.write(f"My best guess is a {guess[0][1]} 🫣")
     if guess[1][0] > 0.2:
-        st.subheader(f"... but it could also be a {guess[1][1]} 😵‍💫")
+        st.write(f"... but it could also be a {guess[1][1]} 😵‍💫")
 
-    #import matplotlib.pyplot as plt
-    #fig, ax = plt.subplots()
-    #plt.imshow(x)
-    #st.pyplot(fig)
+    if show_details:
+        st.header("Steps in calculation 🧮")
+
+        st.subheader('Digit after pre-processing')
+        fig, ax = plt.subplots(figsize=(4, 4))
+        plt.imshow(x, cmap='gray')
+        plt.xticks([])
+        plt.yticks([])
+        ax.axis('off')
+        st.pyplot(fig)
+
+        st.subheader('Probability distribution $P$ over digits $i$ from pre-trained neural network')
+        fig, ax = plt.subplots(figsize=(4,2))
+        ax.bar(height=prediction, x=[f'{i}' for i in range(10)], color='b')
+        plt.xlabel('$i$')
+        plt.ylabel('$P(i)$')
+        st.pyplot(fig)
