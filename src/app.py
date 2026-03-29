5bba50fa5548fc6f9f02327572d421c15ae145d9
magnushelliesen <magnus.helliesen@gmail.com>
2026-03-29 19:10:39 +0200

    Auto

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(initial_sidebar_state="expanded")
+st.set_page_config(initial_sidebar_state="auto")
 
 pg = st.navigation(
     [
--- src/pages/1_ℹ️_About_the_app.py
+++ src/pages/1_ℹ️_About_the_app.py
@@ -13,10 +13,10 @@ st.markdown(
     f"""
     # About the app
     This app is written by [Magnus Kvåle Helliesen](https://github.com/magnushelliesen), mostly for fun.
-    It's hosted on Google Cloud Run, and uses a pre-trained neural network, stored on Google Cloud Storage.
+    It's hosted on Google Cloud Run and uses a pre-trained neural network, which is stored on Google Cloud Storage.
     
-    The code for the app can be found in [this GitHub-repo](https://github.com/magnushelliesen/handwritten-digit-recognizer-app),
-    and the code for the neural network-package can be found in [this GitHub-repo](https://github.com/magnushelliesen/neural-network).
+    The code for the app can be found in this [GitHub-repo](https://github.com/magnushelliesen/handwritten-digit-recognizer-app),
+    and the code for the neural network-package can be found in this [GitHub-repo](https://github.com/magnushelliesen/neural-network).
 
     ## About the neural network
     The neural network has {nn.n_hidden: ,.0f} hidden layers, with {nn.dim_hidden: ,.0f} nodes each.
