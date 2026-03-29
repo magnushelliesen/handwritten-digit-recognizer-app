cafd009f18be1233461e00045e13bd564ccb5e83
magnushelliesen <magnus.helliesen@gmail.com>
2026-03-29 18:41:39 +0200

    Strammet opp utseende

--- src/pages/1_ℹ️_About_the_app.py
+++ src/pages/1_ℹ️_About_the_app.py
@@ -5,21 +5,22 @@ from functions import get_neural_network
 if "nn" in st.session_state:
     nn = st.session_state.nn
 else:
-    nn = get_neural_network()
+    with st.spinner("Fetching neural network, hang on...", show_time=True):
+        nn = get_neural_network()
     st.session_state.nn = nn
 
 st.markdown(
     f"""
     # About the app
-    This app is written by [Magnus Kvåle Helliesen](https://github.com/magnushelliesen).
-    It runs on Google Cloud Run, and reads a pre-trained neural network from Google Cloud Storage.
+    This app is written by [Magnus Kvåle Helliesen](https://github.com/magnushelliesen), mostly for fun.
+    It's hosted on Google Cloud Run, and uses a pre-trained neural network, stored on Google Cloud Storage.
     
     The code for the app can be found in [this GitHub-repo](https://github.com/magnushelliesen/handwritten-digit-recognizer-app),
     and the code for the neural network-package can be found in [this GitHub-repo](https://github.com/magnushelliesen/neural-network).
 
     ## About the neural network
     The neural network has {nn.n_hidden: ,.0f} hidden layers, with {nn.dim_hidden: ,.0f} nodes each.
-    The neural network has been trained showing it {nn.training: ,.0f} random digits from the
+    The neural network has been trained showing it {nn.training: ,.0f} random draws of digits from the
     [MNIST dataset](https://git-disl.github.io/GTDLBench/datasets/mnist_datasets/).
     """
 )
