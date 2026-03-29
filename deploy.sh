0ddf363cf0b53bbdeb1dbfd80121ff971f9a9f57
Magnus Kvåle Helliesen <104299371+magnushelliesen@users.noreply.github.com>
2026-03-29 17:33:52 +0200

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,2 +0,0 @@
-gcloud builds submit --tag gcr.io/neural-network-app-440619/handwritten-digit-recognizer-app .
-gcloud run deploy --image gcr.io/neural-network-app-440619/handwritten-digit-recognizer-app --platform managed --allow-unauthenticated
