0a0dc8d97a8f16ab557a490cdb0792b21f20621e
magnushelliesen <magnus.helliesen@gmail.com>
2026-03-30 16:03:51 +0200

    Fikset bredde osv

--- src/pages/0_✨_Handwritten_digit_recognizer.py
+++ src/pages/0_✨_Handwritten_digit_recognizer.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from streamlit_drawable_canvas import st_canvas  # type: ignore
+from streamlit_drawable_canvas import st_canvas  # pyright: ignore
 
 from backend.backend import return_neural_network
-from backend._0_backend import (
+from backend.backend_0 import (
     recognize_on_click,
     print_guess,
     print_steps,
@@ -13,32 +13,39 @@ nn = return_neural_network()
 
 st.session_state.setdefault("is_guessing", False)
 
+with st.container(width=400):
+    if not st.session_state.is_guessing:
+        st.subheader("Write a digit 🖋️")
+        st.write("Such as 1, 2, ... (It doesn't know letters.)")
 
-if not st.session_state.is_guessing:
-    st.subheader("Write a digit (1, 2, ...) 🖋️")
+        # Accept drawing as user input
+        drawing = st_canvas(
+            stroke_width=40,
+            stroke_color="#000000",
+            background_color="#FFFFFF",
+            width=400,
+            height=400,
+            drawing_mode="freedraw",
+            key="canvas",
+        )
 
-    # Accept drawing as user input
-    drawing = st_canvas(
-        stroke_width=20,
-        stroke_color="#000000",
-        background_color="#FFFFFF",
-        width=250,
-        height=250,
-        drawing_mode="freedraw",
-        key="canvas",
-    )
+        if st.button(
+            "Recognize digit",
+            icon="👀",
+            on_click=recognize_on_click,
+            args=[drawing, nn],
+            width="stretch",
+        ):
+            pass
+    else:
+        st.subheader("Best guess 💡")
+        print_guess()
 
-    if st.button(
-        "Recognize digit", icon="👀", on_click=recognize_on_click, args=[drawing, nn]
-    ):
-        pass
-else:
-    st.subheader("Best guess 💡")
-    print_guess()
+        col_1, col_2 = st.columns(2)
 
-    if st.button("Reset", icon="↩️", on_click=reset_on_click):
-        pass
+        if col_1.button("Go back", icon="↩️", on_click=reset_on_click, width="stretch"):
+            pass
 
-    if st.button("Show nitty gritty details", icon="🧮"):
-        st.subheader("Steps in calculation 🚀")
-        print_steps(nn)
+        if col_2.button("Show details", icon="🧮", width="stretch"):
+            st.subheader("Steps in calculation 🚀")
+            print_steps(nn)
